b945539916ef1620baebf03c064892b1318c980d
sachin <root@LAPTOP-UTILEKND>
2024-12-02 18:07:36 +0530

    added terraform backend

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = ""
+    bucket         = "dev-eks-terraform.state"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
