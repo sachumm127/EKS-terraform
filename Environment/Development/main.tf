36cf159270374d95a887bc029aafd83ba09180e2
sachin.m.m <82070128+sachumm127@users.noreply.github.com>
2024-12-24 12:11:47 +0530

    Update main.tf

--- Environment/Development/main.tf
+++ Environment/Development/main.tf
@@ -29,6 +29,7 @@ module "eks" {
   is_eks_nodegroup_role_enabled = true
   ondemand_instance_types       = var.ondemand_instance_types
 
+
   desired_capacity_on_demand = var.desired_capacity_on_demand
   min_capacity_on_demand     = var.min_capacity_on_demand
   max_capacity_on_demand     = var.max_capacity_on_demand
@@ -38,11 +39,4 @@ module "eks" {
   endpoint-public-access     = var.endpoint-public-access
 
   addons = var.addons
-
-  logging = {
-    cluster_logging = {
-      enable = true
-      types  = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
-    }
-  }
 }
